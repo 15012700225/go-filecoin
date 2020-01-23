de484658110229f4543659904ae830b360ae5a4f
laser <l@s3r.me>
2020-01-22 16:25:25 -0800

    add connector structs

--- internal/app/go-filecoin/retrieval_market_connector/client.go
+++ internal/app/go-filecoin/retrieval_market_connector/client.go
@@ -8,25 +8,20 @@ import (
 	"github.com/filecoin-project/go-fil-markets/shared/types"
 )
 
-// RetrievalClientNodeConnector adapts the node to provide an interface used by the retrieval client.
 type RetrievalClientNodeConnector struct{}
 
-// NewRetrievalClientNodeConnector creates a new connector.
 func NewRetrievalClientNodeConnector() *RetrievalClientNodeConnector {
 	return &RetrievalClientNodeConnector{}
 }
 
-// GetOrCreatePaymentChannel retrieves a payment channel for the retrieval client.
 func (r *RetrievalClientNodeConnector) GetOrCreatePaymentChannel(ctx context.Context, clientAddress address.Address, minerAddress address.Address, clientFundsAvailable tokenamount.TokenAmount) (address.Address, error) {
-	panic("TODO: go-fil-markets integration")
+	panic("implement me")
 }
 
-// AllocateLane creates a lane for the retrieval client.
 func (r *RetrievalClientNodeConnector) AllocateLane(paymentChannel address.Address) (uint64, error) {
-	panic("TODO: go-fil-markets integration")
+	panic("implement me")
 }
 
-// CreatePaymentVoucher creates a payment voucher for the retrieval client.
 func (r *RetrievalClientNodeConnector) CreatePaymentVoucher(ctx context.Context, paymentChannel address.Address, amount tokenamount.TokenAmount, lane uint64) (*types.SignedVoucher, error) {
-	panic("TODO: go-fil-markets integration")
+	panic("implement me")
 }
--- internal/app/go-filecoin/storage_market_connector/provider.go
+++ internal/app/go-filecoin/storage_market_connector/provider.go
@@ -4,190 +4,59 @@ import (
 	"context"
 	"io"
 
-	"github.com/pkg/errors"
-
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/shared/tokenamount"
+	t2 "github.com/filecoin-project/go-fil-markets/shared/types"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-	spasm "github.com/filecoin-project/specs-actors/actors/builtin/storage_market"
-	spaminer "github.com/filecoin-project/specs-actors/actors/builtin/storage_miner"
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"
-
-	"github.com/filecoin-project/go-filecoin/internal/app/go-filecoin/plumbing/msg"
-	"github.com/filecoin-project/go-filecoin/internal/pkg/message"
-	"github.com/filecoin-project/go-filecoin/internal/pkg/piecemanager"
-	"github.com/filecoin-project/go-filecoin/internal/pkg/types"
-	"github.com/filecoin-project/go-filecoin/internal/pkg/vm/abi"
-	fcsm "github.com/filecoin-project/go-filecoin/internal/pkg/vm/actor/builtin/storagemarket"
-	vmaddr "github.com/filecoin-project/go-filecoin/internal/pkg/vm/address"
-	"github.com/filecoin-project/go-filecoin/internal/pkg/wallet"
 )
 
-// StorageProviderNodeConnector adapts the node to provide an interface for the storage provider
-type StorageProviderNodeConnector struct {
-	connectorCommon
+type StorageProviderNodeConnector struct{}
 
-	minerAddr    address.Address
-	chainStore   chainReader
-	outbox       *message.Outbox
-	pieceManager piecemanager.PieceManager
+func NewStorageProviderNodeConnector() *StorageProviderNodeConnector {
+	return &StorageProviderNodeConnector{}
 }
 
-var _ storagemarket.StorageProviderNode = &StorageProviderNodeConnector{}
-
-// NewStorageProviderNodeConnector creates a new connector
-func NewStorageProviderNodeConnector(ma address.Address,
-	cs chainReader,
-	ob *message.Outbox,
-	w *msg.Waiter,
-	pm piecemanager.PieceManager,
-	wg WorkerGetter,
-	wlt *wallet.Wallet,
-) *StorageProviderNodeConnector {
-	return &StorageProviderNodeConnector{
-		connectorCommon: connectorCommon{cs, w, wlt, ob, wg},
-		chainStore:      cs,
-		minerAddr:       ma,
-		outbox:          ob,
-		pieceManager:    pm,
-	}
+func (s *StorageProviderNodeConnector) MostRecentStateId(ctx context.Context) (storagemarket.StateKey, error) {
+	panic("implement me")
 }
 
-// AddFunds sends a message to add storage market collateral for the given address
 func (s *StorageProviderNodeConnector) AddFunds(ctx context.Context, addr address.Address, amount tokenamount.TokenAmount) error {
-	workerAddr, err := s.GetMinerWorker(ctx, s.minerAddr)
-	if err != nil {
-		return err
-	}
-
-	return s.addFunds(ctx, workerAddr, addr, amount)
+	panic("implement me")
 }
 
-// EnsureFunds checks the balance for an account and adds funds to the given amount if the balance is insufficient
 func (s *StorageProviderNodeConnector) EnsureFunds(ctx context.Context, addr address.Address, amount tokenamount.TokenAmount) error {
-	balance, err := s.GetBalance(ctx, addr)
-	if err != nil {
-		return err
-	}
-
-	if !balance.Available.LessThan(amount) {
-		return nil
-	}
-
-	return s.AddFunds(ctx, addr, tokenamount.Sub(amount, balance.Available))
+	panic("implement me")
 }
 
-// PublishDeals publishes storage deals on chain
+func (s *StorageProviderNodeConnector) GetBalance(ctx context.Context, addr address.Address) (storagemarket.Balance, error) {
+	panic("implement me")
+}
+
 func (s *StorageProviderNodeConnector) PublishDeals(ctx context.Context, deal storagemarket.MinerDeal) (storagemarket.DealID, cid.Cid, error) {
-	sig := types.Signature(deal.Proposal.ProposerSignature.Data)
-
-	fcStorageProposal := types.StorageDealProposal{
-		PieceRef:  deal.Proposal.PieceRef,
-		PieceSize: types.Uint64(deal.Proposal.PieceSize),
-
-		Client:   deal.Proposal.Client,
-		Provider: deal.Proposal.Provider,
-
-		ProposalExpiration: types.Uint64(deal.Proposal.ProposalExpiration),
-		Duration:           types.Uint64(deal.Proposal.Duration),
-
-		StoragePricePerEpoch: types.Uint64(deal.Proposal.StoragePricePerEpoch.Uint64()),
-		StorageCollateral:    types.Uint64(deal.Proposal.StorageCollateral.Uint64()),
-
-		ProposerSignature: &sig,
-	}
-	params, err := abi.ToEncodedValues([]types.StorageDealProposal{fcStorageProposal})
-	if err != nil {
-		return 0, cid.Undef, err
-	}
-
-	workerAddr, err := s.GetMinerWorker(ctx, s.minerAddr)
-	if err != nil {
-		return 0, cid.Undef, err
-	}
-
-	mcid, cerr, err := s.outbox.Send(
-		ctx,
-		workerAddr,
-		vmaddr.StorageMarketAddress,
-		types.ZeroAttoFIL,
-		types.NewGasPrice(1),
-		types.NewGasUnits(300),
-		true,
-		fcsm.PublishStorageDeals,
-		params,
-	)
-	if err != nil {
-		return 0, cid.Undef, err
-	}
-
-	receipt, err := s.wait(ctx, mcid, cerr)
-	if err != nil {
-		return 0, cid.Undef, err
-	}
-
-	dealIDValues, err := abi.Deserialize(receipt.Return[0], abi.UintArray)
-	if err != nil {
-		return 0, cid.Undef, err
-	}
-
-	dealIds, ok := dealIDValues.Val.([]uint64)
-	if !ok {
-		return 0, cid.Undef, xerrors.New("decoded deal ids are not a []uint64")
-	}
-
-	if len(dealIds) < 1 {
-		return 0, cid.Undef, xerrors.New("Successful call to publish storage deals did not return deal ids")
-	}
-
-	return storagemarket.DealID(dealIds[0]), mcid, err
+	panic("implement me")
 }
 
-// ListProviderDeals lists all deals for the given provider
 func (s *StorageProviderNodeConnector) ListProviderDeals(ctx context.Context, addr address.Address) ([]storagemarket.StorageDeal, error) {
-	return s.listDeals(ctx, addr)
+	panic("implement me")
 }
 
-// OnDealComplete adds the piece to the storage provider
-func (s *StorageProviderNodeConnector) OnDealComplete(ctx context.Context, deal storagemarket.MinerDeal, pieceSize uint64, pieceReader io.Reader) error {
-	// TODO: storage provider is expecting a sector ID here. This won't work. The sector ID needs to be removed from
-	// TODO: the return value, and storage provider needs to call OnDealSectorCommitted which should add Sector ID to its
-	// TODO: callback.
-	return s.pieceManager.SealPieceIntoNewSector(ctx, deal.DealID, pieceSize, pieceReader)
+func (s *StorageProviderNodeConnector) OnDealComplete(ctx context.Context, deal storagemarket.MinerDeal, pieceSize uint64, pieceReader io.Reader) (uint64, error) {
+	panic("implement me")
 }
 
-// LocatePieceForDealWithinSector finds the sector, offset and length of a piece associated with the given deal id
-func (s *StorageProviderNodeConnector) LocatePieceForDealWithinSector(ctx context.Context, dealID uint64) (sectorNumber uint64, offset uint64, length uint64, err error) {
-	var smState spasm.StorageMarketActorState
-	err = s.chainStore.GetActorStateAt(ctx, s.chainStore.Head(), vmaddr.StorageMarketAddress, &smState)
-	if err != nil {
-		return 0, 0, 0, err
-	}
+func (s *StorageProviderNodeConnector) GetMinerWorker(ctx context.Context, miner address.Address) (address.Address, error) {
+	panic("implement me")
+}
 
-	var minerState spaminer.StorageMinerActorState
-	err = s.chainStore.GetActorStateAt(ctx, s.chainStore.Head(), s.minerAddr, &minerState)
-	if err != nil {
-		return 0, 0, 0, err
-	}
+func (s *StorageProviderNodeConnector) SignBytes(ctx context.Context, signer address.Address, b []byte) (*t2.Signature, error) {
+	panic("implement me")
+}
 
-	for sectorNumber, sectorInfo := range minerState.PreCommittedSectors {
-		for _, deal := range sectorInfo.Info.DealIDs.Items {
-			if uint64(deal) == dealID {
-				offset := uint64(0)
-				for _, did := range sectorInfo.Info.DealIDs.Items {
-					deal, ok := smState.Deals[did]
-					if !ok {
-						return 0, 0, 0, errors.Errorf("Could not find miner deal %d in storage market state", did)
-					}
+func (s *StorageProviderNodeConnector) OnDealSectorCommitted(ctx context.Context, provider address.Address, dealID uint64, cb storagemarket.DealSectorCommittedCallback) error {
+	panic("implement me")
+}
 
-					if uint64(did) == dealID {
-						return uint64(sectorNumber), offset, uint64(deal.Deal.Proposal.PieceSize.Total()), nil
-					}
-					offset += uint64(deal.Deal.Proposal.PieceSize.Total())
-				}
-			}
-		}
-	}
-	return 0, 0, 0, errors.New("Deal not found")
+func (s *StorageProviderNodeConnector) LocatePieceForDealWithinSector(ctx context.Context, dealID uint64) (sectorID uint64, offset uint64, length uint64, err error) {
+	panic("implement me")
 }
